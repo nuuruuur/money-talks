ab9453968f6bdd3fad8ad019b48542dcd9491313
nuuruuur <198761281+nuuruuur@users.noreply.github.com>
2026-04-08 13:58:17 +0700

    GOOD

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App"; // Memanggil file App.jsx 
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
